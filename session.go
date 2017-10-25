d5c03b7689caf715405d79a0f497c7cb9d0b735d
XuWentao <wtao.xu@gmail.com>
2017-10-26 01:46:49 +0800

    set timeout to 3s

--- session.go
+++ session.go
@@ -422,7 +422,7 @@ func (s *session) retry(maxCnt int, infoSchemaChanged bool) error {
 	var err error
 	if s.prelocks != nil {
 		//log.Infof("[XUWT] [%d] lock", connID)
-		timeout, _ := goctx.WithTimeout(s.goCtx, 1 * time.Second)
+		timeout, _ := goctx.WithTimeout(s.goCtx, 3 * time.Second)
 		for _, lock := range s.prelocks {
 			if !lock.Lock(timeout) {
 				return kv.ErrLockConflict
